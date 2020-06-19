35f6c9e1820d2a67c83dd83d55345f4d03642e02
Syauqi Zaidan <zaidanline67@gmail.com>
2020-06-19 19:59:16 +0700

    Optimizing Assets ✌

--- vendor/metronic/assets/vendors/general/inputmask/index.js
+++
@@ -1,5 +0,0 @@
-require("./dist/inputmask/inputmask.extensions");
-require("./dist/inputmask/inputmask.date.extensions");
-require("./dist/inputmask/inputmask.numeric.extensions");
-
-module.exports = require("./dist/inputmask/inputmask.js");
